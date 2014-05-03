98caefb043bb2f74cc0567b3456934be03d2561a
n.raghavendran <n.raghavendran@gmail.com>
2014-05-03 23:09:29 +0530

    Codechef GIT project created and first problem from may 2014 is being committed.

---
+++ may-2014/ChefJumpingOJUMPS/ojumps.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+//#include <climits>
+
+using namespace std;
+
+int main()
+{
+    //cout << "ulonglongmax=" << ULONG_LONG_MAX << endl;
+    unsigned long long int location = 0;
+    while (cin >> location) {
+        if ((location % 3) == 0) {
+            cout << "yes" << endl;
+        }
+        else {
+//            if (location == 1) {
+//                cout << "yes" << endl;
+//            }
+            //else {
+            if (((location - 1) % 6) == 0) {
+                cout << "yes" << endl;
+            }
+            else {
+                cout << "no" << endl;
+            }
+            //}
+        }
+    }
+    return 0;
+}
