1950d6e52468aa2e1138811698a456a1bb35d70f
n.raghavendran <n.raghavendran@gmail.com>
2014-05-03 23:19:05 +0530

    AC :-)

--- may-2014/ChefJumpingOJUMPS/ojumps.cpp
+++ may-2014/ChefJumpingOJUMPS/ojumps.cpp
@@ -12,17 +12,12 @@ int main()
             cout << "yes" << endl;
         }
         else {
-//            if (location == 1) {
-//                cout << "yes" << endl;
-//            }
-            //else {
             if (((location - 1) % 6) == 0) {
                 cout << "yes" << endl;
             }
             else {
                 cout << "no" << endl;
             }
-            //}
         }
     }
     return 0;
