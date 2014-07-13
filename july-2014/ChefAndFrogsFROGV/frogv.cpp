1c90e55a9255f7374885add7b4918810bea44ce3
Raghavendran N <n.raghavendran@gmail.com>
2014-07-13 23:11:43 +0530

    TLE version. Submission ID: 4321181. Changes to the CanFrogsCommunicate method. One key problem was that if the frog1SortedIndex was greater than frog2SortedIndex, then the answer was yes, but could be a no. So, the values have been swapped and checked. Also, the default has changed from true to false. Also, scanf ret val was not checked in the main.

--- july-2014/ChefAndFrogsFROGV/frogv.cpp
+++ july-2014/ChefAndFrogsFROGV/frogv.cpp
@@ -10,6 +10,7 @@ int sortedPositions[MAX_POSITIONS];
 int GetIndexFromSortedPositions(int frogIdx, int start, int end)
 {
 	int pos = positions[frogIdx]; // the actual position as received in the input
+	//printf("Debug: frogIdx= %d and frogPos= %d\n", frogIdx, pos);
 	int mid = 0;
 	while (start <= end) {
 		mid = start + (end - start) / 2;
@@ -28,13 +29,26 @@ int GetIndexFromSortedPositions(int frogIdx, int start, int end)
 
 bool CanFrogsCommunicate(int frog1Idx, int frog2Idx, int n, int k)
 {
-	bool canCommunicate = true;
+	bool canCommunicate = false;
 	int frog1SortedIdx = GetIndexFromSortedPositions(frog1Idx, 1, n);
 	int frog2SortedIdx = GetIndexFromSortedPositions(frog2Idx, 1, n);
+	//printf("Debug: frog1SortedIdx= %d and frog2SortedIdx= %d\n", frog1SortedIdx, frog2SortedIdx);
+	if (frog1SortedIdx == frog2SortedIdx) {
+	    return true;
+	}
+	if (frog1SortedIdx > frog2SortedIdx) {
+	    std::swap(frog1SortedIdx, frog2SortedIdx);
+	}
+	//printf("Debug: After swap, frog1SortedIdx= %d and frog2SortedIdx= %d\n", frog1SortedIdx, frog2SortedIdx);
+	// We care comparing data at i+1 with data at i, so, i < frog2SortedIdx; is correct below.
 	for (int i = frog1SortedIdx; i < frog2SortedIdx; ++i) {
+		//printf("Debug: k= %d pos(i+1)= %d and pos(i)= %d diff= %d\n", k, sortedPositions[i + 1], sortedPositions[i], (sortedPositions[i + 1] - sortedPositions[i]));
 		if ((sortedPositions[i + 1] - sortedPositions[i]) > k) {
 			canCommunicate = false;
 			break;
+		}
+		else {
+		  canCommunicate = true;
 		}
 	}
 	return canCommunicate;
@@ -51,7 +65,7 @@ int main()
 
 	for (int i = 1; i <= n; ++i) {
 		scanf("%d", &positions[i]);
-		//printf("D:%d ", positions[i]);
+		//printf("D:(%d,%d) ", i, positions[i]);
 		sortedPositions[i] = positions[i];
 	}
 
@@ -59,11 +73,17 @@ int main()
 	std::sort(sortedPositions, sortedPositions + n + 1);
 
 	for (int i = 0; i < p; ++i) {
-		scanf("%d %d", &frog1Idx, &frog2Idx);
-		//printf("Debug: %d %d\n", frog1Idx, frog2Idx);
+		if (scanf("%d %d", &frog1Idx, &frog2Idx) == -1)
+		  break;
+
+		//printf("Debug: Actual input= %d %d\n", frog1Idx, frog2Idx);
+
 		if (frog2Idx < frog1Idx){
 			std::swap(frog1Idx, frog2Idx);
 		}
+
+		//printf("Debug: Changed input= %d %d\n", frog1Idx, frog2Idx);
+
 		canCommunicate = CanFrogsCommunicate(frog1Idx, frog2Idx, n, k);
 		if (canCommunicate) {
 			printf("Yes\n");
