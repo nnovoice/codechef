1c3a741955a18da4937411bb389fe1bd3aa57a1c
Raghavendran N <n.raghavendran@gmail.com>
2014-07-20 23:44:15 +0530

    AC with formula

--- july-2014/cookoff/rrsum.cpp
+++ july-2014/cookoff/rrsum.cpp
@@ -12,21 +12,34 @@ int main()
 	unsigned int twice_n = 0;
 
 	scanf ("%u %u", &n, &m);
+	//printf("D:%u %u\n", n, m);
 
 	twice_n = 2 * n;
+	unsigned int num_start = n + 2;
+	unsigned int num_end = 3 * n;
 
-	for (unsigned int i = 1; i <= n; ++i){
-		for (unsigned int j = (n  + 1); j <= twice_n; ++j) {
-			totalMap[i + j] += 1;
-		}
-	}
 
 	for(int i = 0; i < m; ++i) {
 		scanf ("%u", &num_for_query);
+		//printf("D:%u\n", num_for_query);
 
-		count = totalMap[num_for_query];
+		count = 0;
 
-		printf("%u\n", count);
+		if (num_for_query < num_start || num_for_query > num_end) {
+			printf("%u\n", count);
+		}
+		else {
+			if (num_for_query >= num_start && num_for_query <= (twice_n + 1)) {
+				count = num_for_query - (n + 1);
+			}
+			else {
+				count = n - (num_for_query - (twice_n + 1));
+			}
+
+			printf("%u\n", count);
+		}
+
+
 	}
 	return 0;
 }
