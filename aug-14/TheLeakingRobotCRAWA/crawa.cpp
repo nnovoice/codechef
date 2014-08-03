a1d9d9b0c0afd3b71d74a14897f3102a640f07b3
Raghavendran N <n.raghavendran@gmail.com>
2014-08-04 01:35:27 +0530

    untested and unbuilt code. just feeling good to commit.

--- aug-14/TheLeakingRobotCRAWA/crawa.cpp
+++ aug-14/TheLeakingRobotCRAWA/crawa.cpp
@@ -3,31 +3,73 @@
 
 int main()
 {
-	int nCases, x = 0, y = 0, diff = 0;
+	int nCases = 0, x = 0, y = 0, diff = 0;
 
 	scanf("%d", &nCases);
 
 	while(nCases--) {
 		scanf("%d %d", &x, &y);
 
-		if (y < 0) y = -1 * y;
-		if (x < 0) x = -1 * x;
-		
-		if (y % 2 != 0)
-			printf("NO\n");
-		else {
-			diff = y - x;
-			if (diff < 0) diff = -1 * diff; 
-			
-			if (diff == 1)
-				printf("YES\n");
-			else if (diff == 0)
-				printf("YES\n");
+		if (x >= 0) {
+			if (x % 2 == 0) {
+				if (y > 0) {
+					if (y % 2 == 0 && y > x)
+						printf ("YES\n");
+					else
+						printf ("NO\n");
+				}
+				else { 
+					y *= -1;
+					if (y % 2 == 0 && y >= x)
+						printf("YES\n");
+					else
+						printf ("NO\n");
+				}
+			}
 			else {
-				printf("NO\n");
+				if (y >= 0) {
+					if (y >= 0 && y <= (x - 1))
+						printf("YES\n");
+					else if (y > (x + 1) && ((y % 2) == 0))
+						printf ("YES\n");
+					else
+						printf("NO\n");
+				}
+				else {
+					y *= -1;
+					if (y > x && ((y % 2) == 0))
+						printf("YES\n");
+					else if (y <= (x - 1))
+						printf("YES\n");
+					else
+						printf("NO\n");
+				}
+			}
+		}
+		else { // x < 0 
+			x *= -1;
+			if (x % 2 == 0) {
+				if (y > 0 && y <= x)
+					printf("YES\n");
+				else if (y > (x + 1) && ((y % 2) == 0))
+					printf ("YES\n");
+				else
+					printf("NO\n");
+			}
+			else {
+				if (y == 0) 
+					printf ("NO\n");
+				else {
+					if (y < 0) y *= -1;
+					if (y > x && ((y % 2) == 0))
+						printf ("YES\n");
+					else
+						printf("NO\n");
+				}
 			}
 		}
 	}
 
 	return 0;
 }
+
