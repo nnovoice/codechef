364c38830da3c10ff17d7be7c1be1f2f42a93280
Raghavendran N <n.raghavendran@gmail.com>
2014-08-11 14:36:24 +0530

    Got WA for submission id: 4547723. This implementation is based on min heap.

--- aug-14/CleaningTablesCLETAB/cletab.cpp
+++ aug-14/CleaningTablesCLETAB/cletab.cpp
@@ -18,49 +18,56 @@ int main()
   map<int, int> priorities;
   map<int, int> allottedTables;
   priority_queue< ii, vector<ii>, less<ii> > pq;
-  int nCases, n, m, c;
+  int nCases, n, m, c, minTablesToClean = 0;
 
   scanf("%d", &nCases);
   printf("Debug: cases=%d\n", nCases);
 
   while (nCases--) {
 	  scanf("%d %d", &n, &m);
-	  printf("Debug: n=%d m=%d\n", n, m);
+	  //printf("Debug: n=%d m=%d\n", n, m);
 
 	  pq = priority_queue< ii, vector<ii>, less<ii> >();
 	  priorities.clear();
+	  allottedTables.clear();
+	  minTablesToClean = 0;
 
 	  for (int i = 0; i < m; ++i) {
 	    scanf("%d", &c);
-	    printf("%d ", c);
+	    //printf("%d ", c);
 	    orders[i] = c;
 	    priorities[c] += 1;
 	  }
-	  printf("Debug: \n");
+	  //printf("Debug: \n");
 
-	  map<int, int>::iterator iter = priorities.begin();
-	  map<int, int>::iterator iter_end = priorities.end();
-	  while (iter != iter_end) {
-		  pq.push(ii(iter->first, iter->second));
-		  ++iter;
-	  }
-
-	  while (!pq.empty()) {
-		  ii front = pq.top(); pq.pop();
-		  printf("top=%d,%d\n", front.first, front.second);
-	  }
-
-//	  for (int i = 0; i < m; ++i) {
-//	    c = orders[i];
-//	    if (pq.size() == n) {
-//
-//	    }
-//
+//	  map<int, int>::iterator iter = priorities.begin();
+//	  map<int, int>::iterator iter_end = priorities.end();
+//	  while (iter != iter_end) {
+//		  pq.push(ii(iter->first, iter->second));
+//		  ++iter;
 //	  }
 
+//	  while (!pq.empty()) {
+//		  ii front = pq.top(); pq.pop();
+//		  printf("top=%d,%d\n", front.first, front.second);
+//	  }
+
+	  for (int i = 0; i < m; ++i) {
+	    c = orders[i];
+	    //printf("Debug: processing request for a table by: %d\n", c);
+	    if (allottedTables[c] == 1) continue;
+	    if (pq.size() == n) {
+	    	ii front = pq.top(); pq.pop();
+	    	allottedTables.erase(front.first);
+	    	//printf("Debug: %d was politely asked to leave\n", front.first);
+	    }
+	    //printf("Debug: allotted table for %d\n", c);
+	    pq.push(ii(c, priorities[c]));
+	    allottedTables[c] = 1;
+	    ++minTablesToClean;
+	  }
+	  printf("%d\n", minTablesToClean);
   }
-
-
   return 0;
 }
 
