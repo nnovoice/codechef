e98017f16bea4b4f822fefba13e1fc2251e8a175
Raghavendran N <n.raghavendran@gmail.com>
2015-08-13 08:39:23 +0530

    AC

--- aug-15/cookmach.cpp
+++ aug-15/cookmach.cpp
@@ -2,21 +2,15 @@
 using namespace std;
 
 bool is_pow_of_2(long n) {
-    cout << "debug: " << "is_pow_of_2(" << n << ")" << endl;
     long temp = n & ~(n - 1);
-    cout << "debug: " << (n - temp) << endl;
     return ((n - temp) == 0) ? true : false;
 }
 
 long num_ops(long a, long b) {
-    cout << "debug: " << a << "\t" << b << endl;
     long nops = 0;
     while (a != b) {
         nops += 1;
-        cout << "debug: " << a << "\t" << b << "\t" << nops << endl;
         if (is_pow_of_2(a)) {
-            //nops += 1;
-            cout << "debug: " << a << "\t" << b << "\t" << nops << endl;
             if (a < b) a <<= 1;
             else a >>= 1;
         }
